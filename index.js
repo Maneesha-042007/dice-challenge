0d946aa415d2b1833dce1f86bc0188dac38d071c
Chigicherla maneesha <chigicherlamaneesha@gmail.com>
2026-06-10 11:11:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage = "dice" + randomNumber1 + ".png";//dice1.png - dice6.png
-var randomImageSource =  randomDiceImage;//images/dice1.png-images/dice6.png
-var image1 = document.queryselectorall("img")[0];
+var randomImageSource =  "images/" + randomDiceImage;//images/dice1.png-images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImagesource);
+image1.setAttribute("src", randomImageSource);
 var randomNumber2 = Math.floor(Math.random() * 6) +  1;
-var randomimagesource2 = "dice"+ randomNumber2 + ".png";
+var randomimagesource2 = "images/dice"+ randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomimagesource2);
 if (randomNumber1 > randomNumber2) {
     document.queryselector("h1").innerHTML = "🚩 Player 1 Wins!";
