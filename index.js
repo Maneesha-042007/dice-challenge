0d56418541fcc500a865bf00d4bcb7486e813d4c
Chigicherla maneesha <chigicherlamaneesha@gmail.com>
2026-06-10 11:20:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var randomNumber2 = Math.floor(Math.random() * 6) +  1;
 var randomimagesource2 = "dice"+ randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomimagesource2);
 if (randomNumber1 > randomNumber2) {
-    document.queryselector("h1").innerHTML = "🚩 Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
  else if(randomNumber1 < randomNumber2) {
-     document.queryselector("h1").innerHTML = "Player 2 Wins! 🚩";
+     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
  } else {
-     document.queryselector("h1").innerHTML = "Draw!";
+     document.querySelector("h1").innerHTML = "Draw!";
  }
